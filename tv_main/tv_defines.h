218e0441cb6f17182a3bd50a8c75df7b991ad29a
Akshat Dubey <akshatdubey@outlook.com>
2016-03-21 01:59:15 +0800

    added hall effect ex, consolidated main file and added a defines file

---
+++ tv_main/tv_defines.h
@@ -0,0 +1,17 @@
+// file to put all the definitions
+
+#define LOOP_PERIOD 			100
+
+#define MOTOR_LEFT_DIR_PIN			4
+#define MOTOR_LEFT_PWM_PIN			5
+
+#define MOTOR_RIGHT_DIR_PIN			7
+#define MOTOR_RIGHT_PWM_PIN			6
+
+#define SERVO_STEER_PIN			8
+#define STEER_MAX_ANGLE
+#define STEER_STRAIGHT_ANGLE	90
+
+#define IMU_FIELDS				3
+
+#define WHEEL_CIRCUMFERENCE 	20
--- tv_main/tv_main.ino
+++ tv_main/tv_main.ino
@@ -1,47 +1,82 @@
+#include <tv_defines.h>
 #include <TimerOne.h>
+#include <TextFinder.h>
+#include <Servo.h>
 
-#define LOOP_PERIOD 100
+long lastLoop = 0;
+
+int steeringAngle = STEER_STRAIGHT_ANGLE;
+Servo steering;
+
+int leftWheelPWM = 0;
+int rightWheelPWM = 0;
+
+// for wheel speed
 unsigned int speedCounter = 0;
 uint16_t wheelRPM = 0;
- 
-void docount(){  // counts from the speed sensor
+float wheelSpeed = 0;
+
+void count(){  // counts from the speed sensor
   speedCounter++;  // increase +1 the counter value
 } 
  
 void timerIsr(){
   Timer1.detachInterrupt();  //stop the timer
   // Serial.print("Motor Speed: "); 
-  wheelRPM = (speedCounter / 20);  // divide by number of holes in Disc
+  wheelRPM = (speedCounter);  // divide by number of holes in disk if any
   // Serial.print(rotation,DEC);  
   // Serial.println(" Rotation per seconds"); 
   speedCounter=0;  //  reset counter to zero
-  Timer1.attachInterrupt( timerIsr );  //enable the timer
+  Timer1.attachInterrupt(timerIsr);  //enable the timer
 }
-long lastLoop = 0;
+
+// for IMU1
+TextFinder  finder1(Serial2); 
+float rpy1[NUMBER_OF_FIELDS];    // array holding values for all the fields
+
+
+// for IMU2
+TextFinder  finder2(Serial3); 
+float rpy2[NUMBER_OF_FIELDS];    // array holding values for all the fields
+
 
 void setup(){
-  Serial.begin(115200);
-  Serial2.begin(115200);
+  //init serial
+  Serial.begin(115200); //for serial output
+  Serial2.begin(57600); //for IMU1
+  Serial3.begin(57600); //for IMU2
 
   //for speed measurement
-  Timer1.initialize(1000000); // set timer for 1sec
-  attachInterrupt(0, docount, RISING);  // increase counter when speed sensor pin goes High
+  Timer1.initialize(LOOP_PERIOD * 1000); // set timer for LOOP_PERIOD
+  attachInterrupt(0, count, RISING);  // increase counter when speed sensor pin goes High
   Timer1.attachInterrupt( timerIsr ); // enable the timer
+
+  //for steering servo
+  steering.attach(SERVO_STEER_PIN);
+
+  //for rear wheel motors
+  pinMode(MOTOR_LEFT_DIR_PIN, OUTPUT);   
+  pinMode(MOTOR_RIGHT_DIR_PIN, OUTPUT); 
+
+  delay(500);
+  initIMUs();
 }
 
 void loop(){
   long now = millis();
+  //loop every 100 ms
   if(now-lastLoop >= LOOP_PERIOD ){
     lastLoop = now;
-  //loop every 100 ms
 
-  //get sensor data
-  getYaw();
+    //get sensor data
+    getYaw();
+
     //wheel speed
-
+    wheelSpeed = WHEEL_CIRCUMFERENCE * wheelRPM * (1000/LOOP_PERIOD)
     //steering angle
-  getSteeringAngle();
-    //yaw
+
+    getSteeringAngle(); // for later if wanna input steering angle
+    steering.write(steeringAngle);
    //write sensor data to SD
 
     //calculate intended yaw
@@ -59,22 +94,74 @@ void loop(){
     //use a look up table to get the diff torque
     //apply and then switch to PID
 
+    torqueVector();
+
    //write error value to SD
   }
 }
 
-void getYaw(){
-  
-}
-void getSteeringAngle(){
+/********************************/
+/*** Initialize IMUs
+/********************************/
 
+void initIMUs()
+{
+  // Output angles in TEXT format & Turn off continuous streaming output & Disable error message output
+  Serial2.write("#ot#o0#oe0");
+  Serial3.write("#ot#o0#oe0");
+  Serial2.flush();
+  Serial3.flush();
+}
+
+
+void getYaw(){
+    Serial2.write("#f");
+    Serial3.write("#f");
+    
+    /************************************************************/
+    /*** Get the IMU values from both IMUs
+    /************************************************************/
+
+    // the current field being received
+    int fieldIndex = 0;            
+
+    // search the Serial Buffer as long as the header character is found
+    boolean found_HeaderChar = finder1.find("H");
+
+    if (found_HeaderChar)
+    {
+      // Get all 3 values (yaw, pitch, roll) from the Serial Buffer
+      while(fieldIndex < IMU_FIELDS)
+      {
+        rpy1[fieldIndex++] = finder1.getFloat();
+      }
+    }
+    
+    // reset the current field being received
+    fieldIndex = 0;            
+
+    // search the Serial Buffer as long as the header character is found
+    found_HeaderChar = finder2.find("H");
+
+    if (found_HeaderChar)
+    {
+      // Get all 3 values (yaw, pitch, roll) from the Serial Buffer
+      while(fieldIndex < IMU_FIELDS)
+      {
+        rpy2[fieldIndex++] = finder2.getFloat();
+      }
+    }  
 }
 
 void calculateYaw(){
-  
+  //use steeringAngle and wheelSpeed
 }
 
 void calculateYawError(){
   
 }
 
+void torqueVector(){
+  analogWrite(leftWheelPWM, value);   //PWM Speed Control
+  analogWrite(rightWheelPWM, value);   //PWM Speed Control
+}
